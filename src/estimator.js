4d3f2b303c158dc4f4d4448214a5b392d95d5878
purplesmile3 <puritychepkurui5@gmail.com>
2020-04-09 09:54:02 +0300

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,11 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+const covid19ImpactEstimator = (reportedCases) => {
+  const currentlyInfected = reportedCases * 10;
+  const severeImpact = currentlyInfected * 50;
+
+  const infectionsByRequestedTime = currentlyInfected * 1024;
+};
 
 export default covid19ImpactEstimator;
