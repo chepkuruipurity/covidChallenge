9d3d47ac73db114ae3063bbf471492d7d8368ceb
purplesmile3 <puritychepkurui5@gmail.com>
2020-04-11 22:04:15 +0300

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -47,12 +47,12 @@ const severelySevereCasesByRequestedTime= Math.floor(0.15 * severeInfectionsByRe
 const hospitalBedsByRequestedTime = Math.trunc(availableBeds - severeCasesByRequestedTime);
 const severeHospitalBedsByRequestedTime= Math.trunc(availableBeds - severelySevereCasesByRequestedTime);
 
-const casesForICUByRequestedTime = (0.05 * infectionsByRequestedTime);
-const severecasesForICUByRequestedTime = (0.05 * severeInfectionsByRequestedTime);
+const casesForICUByRequestedTime = Math.trunc(0.05 * infectionsByRequestedTime);
+const severecasesForICUByRequestedTime = Math.trunc(0.05 * severeInfectionsByRequestedTime);
 
 
-const casesForVentilatorsByRequestedTime = (0.02 * infectionsByRequestedTime);
-const severecasesForVentilatorsByRequestedTime = (0.02 * severeInfectionsByRequestedTime);
+const casesForVentilatorsByRequestedTime = Math.trunc(0.02 * infectionsByRequestedTime);
+const severecasesForVentilatorsByRequestedTime = Math.trunc(0.02 * severeInfectionsByRequestedTime);
 
 const dollarsInFlight= Number(Math.trunc(infectionsByRequestedTime* moneyFlight));
 const severedollarsInFlight=Number(Math.trunc(severeInfectionsByRequestedTime* moneyFlight));
