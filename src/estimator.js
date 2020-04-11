a04a34b3f108aad5405ff350a7bb06c70a7fbba3
purplesmile3 <puritychepkurui5@gmail.com>
2020-04-11 23:23:51 +0300

    challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -54,8 +54,8 @@ const severecasesForICUByRequestedTime = Math.floor(0.05 * severeInfectionsByReq
 const casesForVentilatorsByRequestedTime = Math.floor(0.02 * infectionsByRequestedTime);
 const severecasesForVentilatorsByRequestedTime = Math.floor(0.02 * severeInfectionsByRequestedTime);
 
-const dollarsInFlight= Number((infectionsByRequestedTime* moneyFlight).toFixed(2));
-const severedollarsInFlight= Number((severeInfectionsByRequestedTime* moneyFlight).toFixed(2));
+const dollarsInFlight= Number((infectionsByRequestedTime* moneyFlight).toFixed(0));
+const severedollarsInFlight= Number((severeInfectionsByRequestedTime* moneyFlight).toFixed(0));
 
 
 return  { 
